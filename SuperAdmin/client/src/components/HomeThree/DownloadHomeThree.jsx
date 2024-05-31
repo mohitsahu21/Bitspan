0385518d24d37a475e79c0658389d45a4825d1af
student demo <anshkanha98@gmail.com>
2024-05-31 18:50:03 +0530

    work on super admin Home 3

--- SuperAdmin/client/src/components/HomeThree/DownloadHomeThree.jsx
+++ SuperAdmin/client/src/components/HomeThree/DownloadHomeThree.jsx
@@ -1,41 +1,48 @@
-import React from 'react';
-import downloadThumb from '../../assets/images/download-thumb-1.png';
-import downloadThumbTwo from '../../assets/images/download-thumb-2.png';
+import React from "react";
+import downloadThumb from "../../assets/images/download-thumb-1.png";
+import downloadThumbTwo from "../../assets/images/download-thumb-2.png";
 
 function DownloadHomeThree({ className }) {
-    return (
-        <>
-            <section className={`appie-download-3-area pt-100 ${className || ''}`} id="download">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="appie-section-title text-center">
-                                <h3 className="appie-title">Download app today!</h3>
-                                <p>Download app for Andraoid today — it's free.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <div
-                                className="appie-download-3-box mt-30 mr-20 wow animated fadeInLeft"
-                                data-wow-duration="2000ms"
-                                data-wow-delay="200ms"
-                            >
-                                <div className="content">
-                                    <h4 className="title">Android</h4>
-                                    <p>Download app for Android today — it's free.</p>
-                                    <a className="main-btn" href="#">
-                                        <i className="fab fa-google-play" />
-                                        Download for Android
-                                    </a>
-                                </div>
-                                <div className="thumb text-center">
-                                    <img src={downloadThumb} alt="" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6">
+  return (
+    <>
+      <section
+        className={`appie-download-3-area pt-25 mb-5 ${className || ""}`}
+        id="download"
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="appie-section-title text-center">
+                <h3 className="appie-title">Download app today!</h3>
+                <p>
+                  Building your Apps busines helps attract more potential
+                  clients. Our integrated marketing team will work directly
+                  long-term high-impact convergence.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-6 offset-lg-3">
+              <div
+                className="appie-download-3-box mt-30 mr-20 wow animated fadeInLeft"
+                data-wow-duration="2000ms"
+                data-wow-delay="200ms"
+              >
+                <div className="content">
+                  <h4 className="title">Android</h4>
+                  <p>Download Apps Managing Business</p>
+                  <a className="main-btn" href="#">
+                    <i className="fab fa-google-play" />
+                    Download for Android
+                  </a>
+                </div>
+                {/* <div className="thumb text-center">
+                  <img src={downloadThumb} alt="" />
+                </div> */}
+              </div>
+            </div>
+            {/* <div className="col-lg-6">
                             <div
                                 className="appie-download-3-box mt-30 ml-20 wow animated fadeInRight"
                                 data-wow-duration="2000ms"
@@ -53,12 +60,12 @@ function DownloadHomeThree({ className }) {
                                     <img src={downloadThumbTwo} alt="" />
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+                        </div> */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default DownloadHomeThree;
