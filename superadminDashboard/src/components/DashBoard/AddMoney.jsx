a5a4e52891a66b41c0a017d866d4842e60eb905b
mohitsahu21 <mohitsahu1993@gmail.com>
2025-01-20 20:32:24 +0530

    update add money using pg.

--- superadminDashboard/src/components/DashBoard/AddMoney.jsx
+++ superadminDashboard/src/components/DashBoard/AddMoney.jsx
@@ -1,12 +1,149 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { MdOutlineFormatListNumbered } from "react-icons/md";
 import { FaMobileAlt } from "react-icons/fa";
 import { RiMarkPenLine } from "react-icons/ri";
 import { FaRupeeSign } from "react-icons/fa";
 import { BiHomeAlt } from "react-icons/bi";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
+import {  fetchWalletBalance } from "../../redux/user/userSlice";
 const AddMoney = () => {
+   const dispatch = useDispatch();
+  const { currentUser, token } = useSelector((state) => state.user);
+  const { walletBalance } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const fullUrl = window.location.href;
+  console.log(fullUrl)
+  const [formData, setFormData] = useState({
+    user_id: currentUser.userId,
+    amount: "",
+    userName: currentUser.username,
+    userPhone: currentUser.ContactNo,
+    userEmail: currentUser.email,
+    userRole: currentUser.role,
+    Payment_Mode: "",
+    website: fullUrl,
+  });
+
+
+  useEffect(()=>{
+    if (currentUser?.userId) {
+            dispatch(fetchWalletBalance(currentUser.userId));
+          }
+  },[])
+
+
+  const [receiptAttachment, setReceiptAttachment] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  console.log(formData)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // setFormData({ ...formData, [name]: value });
+    if (name === "amount") {
+      if (/^\d*$/.test(value)) {
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+      }
+    }
+    else {
+      setFormData({
+        ...formData,
+        [name] : value,
+      });
+    }
+  };
+
+  const handleFileChange = (e) => {
+    setReceiptAttachment(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(formData.amount < 100){
+      Swal.fire({
+        title: "Error",
+        text:
+         "Minimum amount is 100",
+        icon: "error",
+      });
+      return
+    }
+    setIsLoading(true);
+    try {
+      const data = new FormData();
+      // Append form fields
+      for (const key in formData) {
+        data.append(key, formData[key]);
+      }
+      // Append file
+      if (receiptAttachment) {
+        data.append("Receiept_Attechment", receiptAttachment);
+      }
+
+      // const response = await axios.post(
+      //   "https://bitspan.vimubds5.a2hosted.com/api/auth/retailer/add-money-wallet",
+      //   data,
+      //   {
+      //     headers: { "Content-Type": "multipart/form-data" },
+      //   }
+      // );
+      const response = await axios.post(
+        "https://bitspan.vimubds5.a2hosted.com/api/auth/upiwf/createOrderToAddWalletMoney",
+        // "http://localhost:7777/api/auth/superAdmin/createOrderToAddWalletMoney",
+        formData
+      );
+    console.log(response)
+      // alert(response.data.message);
+     
+      if(response.data.status){
+        Swal.fire({
+          title: "Payment Link Created Successfully",
+          text: `Your Order Id is ${response.data.data.result.orderId}`,
+          icon: "success",
+        }).then(() => {
+          // Navigate to the payment URL after closing the alert
+          // window.location.href = response.data.data.result.payment_url; 
+          window.open(response.data.data.result.payment_url, "_blank");
+        });
+        // navigate(`/${response.data.data.result.payment_url}`)
+        setFormData({
+          user_id: currentUser.userId,
+          amount: "",
+          userName: currentUser.username,
+          userPhone: currentUser.ContactNo,
+          userEmail: currentUser.email,
+          userRole: currentUser.role,
+          Payment_Mode: "",
+          website: fullUrl,
+        });
+
+      }
+
+      // Swal.fire({
+      //   title: "Form Submitted Successfully",
+      //   text: response.data.message,
+      //   icon: "success",
+      // });
+      
+    } catch (error) {
+      console.error("Error submitting form:", error.response.data);
+      alert("Failed to submit form. Please try again.");
+      Swal.fire({
+        title: "Error",
+        text:
+          error.response?.data || "Failed to submit form. Please try again.",
+        icon: "error",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <>
       <Wrapper>
@@ -37,6 +174,7 @@ const AddMoney = () => {
                       </div>
                     </div>
                   </div>
+                  <form onSubmit={handleSubmit}>
                   <div className="row justify-content-lg-end justify-content-center pe-lg-4">
                     <div className="col-xxl-9 col-xl-9 col-lg-9 col-md-10 col-sm-10 shadow bg-body-tertiary rounded m-4 px-3 py-5">
                       <div className="row d-flex flex-column g-4">
@@ -52,7 +190,7 @@ const AddMoney = () => {
                                 id="floatingInputGroup1"
                                 placeholder="Transaction Number"
                                 readOnly
-                                value="ASHI6462"
+                                value={currentUser.userId}
                               />
                               <label for="floatingInputGroup1">
                                 Your User Id
@@ -71,7 +209,12 @@ const AddMoney = () => {
                                 type="text"
                                 class="form-control"
                                 id="floatingInputGroup2"
-                                placeholder="Mobile Number"
+                                placeholder="Enter Amount"
+                                name="amount"
+                                value={formData.amount}
+                                onChange={handleChange}
+                                required
+                                min={100}
                               />
                               <label for="floatingInputGroup2">
                                 Amount in Rs. {"(Min 100/-)"}
@@ -81,7 +224,7 @@ const AddMoney = () => {
                         </div>
                         <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                           <p className="fw-bold">
-                            Wallet Available Amount : Rs. 200.00
+                            Wallet Available Amount : Rs. {walletBalance}
                           </p>
                         </div>
 
@@ -91,9 +234,13 @@ const AddMoney = () => {
                               class="form-select"
                               id="floatingSelect"
                               aria-label="Floating label select example"
+                              name="Payment_Mode"
+                                value={formData.Payment_Mode}
+                                onChange={handleChange}
+                                required
                             >
-                              <option selected>Select Payment Method</option>
-                              <option value="UPI">UPI</option>
+                              <option selected value="">Select Payment Method</option>
+                              <option value="Online">Online</option>
                               {/* <option value="2">
                                 Debit Card/ Credit Card / Netbanking
                               </option> */}
@@ -104,20 +251,25 @@ const AddMoney = () => {
 
                         <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                           <p className="m-0">UPI Payment - Free</p>
-                          <p className="m-0">
+                          {/* <p className="m-0">
                             Debit Card/ Credit Card / Netbanking - 3% Additional
                             Charges
-                          </p>
+                          </p> */}
                         </div>
 
                         <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                           <div className="text-start mb-3">
-                            <button className="btn p-2">Pay Now</button>
+                          <button className="btn p-2" disabled={isLoading}>
+                                {isLoading
+                                  ? "Processing..."
+                                  : "Pay Now"}
+                              </button>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
+                  </form>
                 </div>
               </div>
             </div>
