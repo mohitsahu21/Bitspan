b5e1e0f32ef16e3dc7797d89492dd3cf03ea12d9
mohitsahu21 <mohitsahu1993@gmail.com>
2025-02-25 17:38:58 +0530

    update login page and app.js

--- superadminDashboard/src/pages/Payment.jsx
+++ superadminDashboard/src/pages/Payment.jsx
@@ -1,24 +1,29 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import Swal from 'sweetalert2';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-const Payment = ({user}) => {
+const Payment = () => {
+   const location = useLocation();
+   const navigate = useNavigate();
      const [isLoading, setIsLoading] = useState(false);
+      // Access the data passed from the previous component
+    const { user } = location.state || {};
     console.log(user)
     // const fullUrl = window.location.href;
     const fullUrl = window.location.origin;
+    
   console.log(fullUrl)
-    const [formData, setFormData] = useState({
-        userId: user.UserId,
-        total_amount: user.amount,
-        userPhone: user.ContactNo,
-      payment_method: user.paymentMode,
-      website: fullUrl,
-    });
+  const [formData, setFormData] = useState({
+    userId: user.userId + "_" + Date.now(),
+    total_amount: user.amount,
+    userPhone: user.ContactNo,
+  payment_method: user.paymentMode,
+  website: fullUrl,
+});
 
     const handleSubmit = async (e) => {
         // e.preventDefault();
-       
         setIsLoading(true);
         try {
           const data = new FormData();
@@ -55,6 +60,7 @@ const Payment = ({user}) => {
               // Navigate to the payment URL after closing the alert
               // window.location.href = response.data.data.result.payment_url; 
               window.open(response.data.data.result.payment_url, "_blank");
+              navigate(`/`)
             });
             // navigate(`/${response.data.data.result.payment_url}`)
             // setFormData({
@@ -79,21 +85,32 @@ const Payment = ({user}) => {
         } catch (error) {
           console.error("Error submitting form:", error.response.data);
           alert("Failed to submit form. Please try again.");
-          Swal.fire({
-            title: "Error",
-            text:
-              error.response?.data || "Failed to submit form. Please try again.",
-            icon: "error",
-          });
+          // Swal.fire({
+          //   title: "Error",
+          //   text:
+          //     error.response?.data || "Failed to submit form. Please try again.",
+          //   icon: "error",
+          // });
+          
         } finally {
           setIsLoading(false);
         }
       };
+  
 useEffect(()=>{
+  if(!user || !user.userId|| !user.amount || !user.ContactNo || !user.paymentMode || !fullUrl){
+    Swal.fire({
+            title: "Error",
+            text: "Failed to submit form. Please try again.",
+            icon: "error",
+          }).then(()=>{
+            navigate('/')
+          })
+  }
 handleSubmit()
 },[])
   return (
-    <div>Payment</div>
+    <div>Payment Processing...</div>
   )
 }
 
